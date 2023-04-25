2824e4fc86776a53da0441d522a73f2dea4946b9
Tianxiang Ren <ren.tianx@northeastern.edu>
2023-04-25 16:17:22 -0700

    updated apis

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 // const baseURL = "http://127.0.0.1:5000/api/v1/";
-const baseURL = "http://movieprox-api-rentxiang.vercel.app/api/v1/";
+const baseURL = "http://movieprox-api-git-master-rentxiang.vercel.app/api/v1/";
 
 
 const privateClient = axios.create({
